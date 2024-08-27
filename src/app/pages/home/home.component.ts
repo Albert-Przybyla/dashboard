6432fbd2a10648d4bd461066b8ad156c8559f614
APrzybyla <106275293+Albert-Przybyla@users.noreply.github.com>
2024-08-28 00:41:23 +0200

    loyalty card on home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { OrdersComponent } from './orders/orders.component';
+import { ProgressBarComponent } from '../../components/progress-bar/progress-bar.component';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [OrdersComponent],
+  imports: [OrdersComponent, ProgressBarComponent, CurrencyPipe],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  protected copy() {}
+}
