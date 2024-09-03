1329c6f466737c6f541da2a9994c19085206891a
APrzybyla <106275293+Albert-Przybyla@users.noreply.github.com>
2024-09-03 10:57:44 +0200

    style fixes

--- src/app/pages/home/orders/orders.component.ts
+++ src/app/pages/home/orders/orders.component.ts
@@ -6,6 +6,14 @@ import { CommonModule, DatePipe } from '@angular/common';
 import { BaseDataComponent } from '../../../base/baseDataComponent';
 import { Observable } from 'rxjs';
 import { status } from '../../../enums/status.enum';
+import {
+  trigger,
+  transition,
+  query,
+  animateChild,
+  animate,
+  style,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-orders',
@@ -13,6 +21,21 @@ import { status } from '../../../enums/status.enum';
   imports: [DataDisplayerComponent, DatePipe, CommonModule],
   templateUrl: './orders.component.html',
   styleUrl: './orders.component.scss',
+  animations: [
+    trigger('fadeOut', [
+      transition(':leave', [
+        query(':leave', animateChild(), { optional: true }),
+        animate('150ms ease-in', style({ opacity: 0 })),
+      ]),
+    ]),
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        query(':enter', animateChild(), { optional: true }),
+        animate('250ms 250ms ease-out', style({ opacity: 1 })),
+      ]),
+    ]),
+  ],
 })
 export class OrdersComponent extends BaseDataComponent<Order> {
   private _tmpDataService = inject(TmpDataService);
